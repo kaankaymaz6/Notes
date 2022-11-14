695a4fad4e2ad8eabcd9543c9429aa301078e250
user <onikaankaymaz@gmail.com>
2022-11-14 23:34:18 +0300

    14.11.2022

---
+++ src/day28/Kare.java
@@ -0,0 +1,19 @@
+package day28;
+
+public class Kare extends Sekil{
+    @Override
+    public int alanHesapla() {
+        return super.alanHesapla();
+    }
+
+    @Override
+    public int cevreHesapla() {
+        return super.cevreHesapla();
+    }
+
+    @Override
+    public int getAcilarToplami() {
+        return super.getAcilarToplami();
+    }
+}
+}
