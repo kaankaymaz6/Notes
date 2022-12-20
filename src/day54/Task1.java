c5aa880397382fde5d22bf39decb1af469ed95d0
user <onikaankaymaz@gmail.com>
2022-12-20 22:29:31 +0300

    20.12.2022

---
+++ src/day54/Task1.java
@@ -0,0 +1,40 @@
+package day54;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Task1 {
+    public static void main(String[] args) {
+        //1.
+        ArrayList<Integer> numbers=new ArrayList<>(List.of(1,2,3,4,5,6,7,8,9,10));
+        //2.
+        ArrayList<Integer> oddNumbers=new ArrayList<>();
+        ArrayList<Integer> evenNumbers=new ArrayList<>();
+        for (var item:numbers){
+            if (item%2==0){
+                evenNumbers.add(item);
+            }else {
+                oddNumbers.add(item);
+            }
+        }
+        //3.
+        for (var item:numbers){
+            System.out.print(item+" ");
+        }
+        Iterator<Integer> iterator = oddNumbers.iterator();
+        //if (iterator.hasNext()) System.out.println(iterator.next());
+        System.out.println();
+        while (iterator.hasNext()){
+            System.out.print(iterator.next()+" ");
+        }
+        System.out.println();
+        System.out.println("Çift Sayılar :"+evenNumbers);
+
+        for (int i =0; i < numbers.size(); i++){
+            if (numbers.get(i)%2!=0){
+                numbers.remove(i);
+            }
+        }
+    }
+}
